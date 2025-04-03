a77a783b12b37919a2d320c8c7d4bc2266c6efaa
LeiCraft_ <88381277+LeiCraft@users.noreply.github.com>
2025-04-03 10:00:04 +0000

    updated to 1.0.16

--- src/uint/index.ts
+++ src/uint/index.ts
@@ -2,4 +2,9 @@
 export * from "./uint.js";
 export * from "./fixed-uint.js";
 export * from "./map.js";
-export { UintUtils, type NumberLike, type BinLike } from "./utils.js";
+export {
+    UintUtils,
+    type NumberLike,
+    type BinLike,
+    type New as NewUint,
+} from "./utils.js";
